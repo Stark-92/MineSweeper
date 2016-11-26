d4b3f1afaf3489b24b12b79cda03f20a3010f966
Abd alhadi Al-Tinawi <abdalhadi.altinawi@gmail.com>
2016-11-26 01:07:09 +0100

    Init

--- MineSweeper/MineSweeper/App.cs
+++ MineSweeper/MineSweeper/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MineSweeper.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,22 +13,23 @@ namespace MineSweeper
         public App()
         {
             // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "MineSweeper",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            //var content = new ContentPage
+            //{
+            //    Title = "MineSweeper",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
 
-            MainPage = new NavigationPage(content);
+            //  MainPage = new NavigationPage(new GameView());
+            MainPage = new GameView();
         }
 
         protected override void OnStart()
---
+++ MineSweeper/MineSweeper/View/GameView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace MineSweeper.View
+{
+    public partial class GameView : ContentPage
+    {
+
+        public GameView()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SingleTap(object sender, EventArgs e)
+        {
+            var cell = (Label) sender;
+            cell.Text = "ONE!!!!";
+        }
+
+        private void DoubleTap(object sender, EventArgs e)
+        {
+            var cell = (Label)sender;
+            cell.Text = "TWO!!!!";
+        }
+        
+    }
+        
+
+    
+}
